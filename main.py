9fe2c6e73fb66c45301b952e4bf01aee682ee44f
Bhany <bhany8@gmail.com>
2022-02-05 17:02:09 -0800

    basic pydantic model init

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-from fastapi import FastAPI
+from typing import Optional
+from fastapi import FastAPI, Query
+import schemas.shipment as s
 
-app = FastAPI()
+app = FastAPI(title="TODO TITLE", openapi_url="/openapi.json")
 
-
+# TODO: create landing page
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.get('/{query_type}/{id}')
+async def read_item(query_type: s.QueryGetType, id: Optional[str]=Query(None, )): # str data validation
+    return {s.QueryGetType(query_type): id}
+
+@app.post('/shipment/')
+async def create_item(item: s.Shipment): 
+    return "ok"
+
+@app.put('/organization/')
+async def create_item(item: s.Organization):
+    return item
